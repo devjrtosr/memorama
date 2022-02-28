9984bbda3afe2d783edaccad5da2700baf525d5e
Keilin99 <mayopb99@gmail.com>
2022-02-28 15:02:43 -0600

    Agregando carpeta llamada memorama-Keilin que contiene el archivo index.html, styles.css y app.js con el ejercicio solicitado

---
+++ memorama-Keilin/app.js
@@ -0,0 +1,73 @@
+const imagenesDiv = [
+  {
+    elementoDiv: "div",
+    claseDiv: "contenedor-tarjeta",
+  },
+  {
+    elementoDiv: "div",
+    claseDiv: "contenedor-tarjeta",
+  },
+  {
+    elementoDiv: "div",
+    claseDiv: "contenedor-tarjeta",
+  },
+  {
+    elementoDiv: "div",
+    claseDiv: "contenedor-tarjeta",
+  },
+  {
+    elementoDiv: "div",
+    claseDiv: "contenedor-tarjeta",
+  },
+  {
+    elementoDiv: "div",
+    claseDiv: "contenedor-tarjeta",
+  },
+  {
+    elementoDiv: "div",
+    claseDiv: "contenedor-tarjeta",
+  },
+  {
+    elementoDiv: "div",
+    claseDiv: "contenedor-tarjeta",
+  },
+  {
+    elementoDiv: "div",
+    claseDiv: "contenedor-tarjeta",
+  },
+  {
+    elementoDiv: "div",
+    claseDiv: "contenedor-tarjeta",
+  },
+  {
+    elementoDiv: "div",
+    claseDiv: "contenedor-tarjeta",
+  },
+  {
+    elementoDiv: "div",
+    claseDiv: "contenedor-tarjeta",
+  },
+  {
+    elementoDiv: "div",
+    claseDiv: "contenedor-tarjeta",
+  },
+  {
+    elementoDiv: "div",
+    claseDiv: "contenedor-tarjeta",
+  },
+  {
+    elementoDiv: "div",
+    claseDiv: "contenedor-tarjeta",
+  },
+  {
+    elementoDiv: "div",
+    claseDiv: "contenedor-tarjeta",
+  },
+];
+
+imagenesDiv.map((etiquetaDiv) => {
+  const elementoDiv = document.createElement(etiquetaDiv.elementoDiv);
+  elementoDiv.classList.add(etiquetaDiv.claseDiv);
+  document.querySelector("#contenedor").appendChild(elementoDiv);
+});
+cd
