44e8b7e85bee34dc7a909bc3955e61006abe64b9
Guelo <zalastft@gmail.com>
2022-03-05 11:22:20 -0600

    Agregando stylos de memorama cambiando cover.jpg centrando el memorama

--- Memorama-Migue/app.js
+++ Memorama-Migue/app.js
@@ -1,21 +1,29 @@
-const cartas = [
-  {
-    elemntoboton: "div",
-    clasesboton: ["botonfila"],
-    fondo: "/assets/fondocartas/rune.png",
-  },
-  {
-    elemntoboton: "div",
-    clasesboton: ["botonfila"],
-    fondo: "/assets/fondocartas/rune.png",
-  },
-];
+let cartas;
 
-cartas.map((carta) => {
-  const fila = document.createElement(carta.elemntoboton);
-  const imagenCarta = document.createElement("img");
-  imagenCarta.classList.add(...carta.clasesboton);
-  imagenCarta.src = carta.fondo;
-  document.querySelector("#Primerafila").appendChild(fila);
-  document.querySelector("#Primerafila").appendChild(imagenCarta);
-});
+const contenedor = document.querySelector("#contenedor");
+const contenedorCartas = document.querySelector("#contenedor-cartas");
+
+const iniciarJuego = () => {
+  window.addEventListener("DOMContentLoaded", () => {
+    //console.log("Este evento esta cargado");
+    cargarCartas();
+  });
+};
+
+document.addEventListener("click", () => {});
+
+const cargarCartas = async () => {
+  const respuesta = await fetch("cartas.json");
+  const data = await respuesta.json();
+  data.sort(() => Math.random() - 0.5);
+  let cartahtml = "";
+  data.map((item) => {
+    cartahtml += ` <div>
+                        <img class="card" src=${item.src}>
+                      </div>
+    `;
+    contenedorCartas.innerHTML = cartahtml;
+  });
+};
+
+iniciarJuego();
